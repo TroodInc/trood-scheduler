651bd26c86a4e41fc79401e407b31ac0cb830dd9
Alexander Gor <ornot.work@gmail.com>
2018-07-26 12:19:21 +0300

    T3-313. Basic task for http calls

---
+++ tasks/base.py
@@ -0,0 +1,30 @@
+import requests
+from requests import RequestException
+from celery.task import task
+
+from trood_auth_client.authentication import get_service_token
+
+
+@task()
+def simple_http_call(url, method="get", use_auth=True, **kwargs):
+    try:
+        headers = {}
+        if use_auth:
+            headers = {"Authorization": get_service_token()}
+
+        response = requests.request(method, url, headers=headers, **kwargs)
+        response.raise_for_status()
+
+        result = {
+            'status': response.status_code,
+            'responce': response.json()
+        }
+
+    except RequestException as e:
+        result = {
+            'status': 'Failed with exception',
+            # 'response': e.response,
+            # 'request': e.request
+        }
+
+    return result
