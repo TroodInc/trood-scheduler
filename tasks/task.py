ca67118a545a70c578c1e4b964af02195eecd7dd
Alexander Gor <ornot.work@gmail.com>
2018-09-12 00:33:10 +0300

    T3-391. Task for expiring tasks notifications

--- tasks/task.py
+++ tasks/task.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import requests
 from django.conf import settings
 from scheduler.celery import app
 from custodian.client import Client
@@ -19,3 +21,37 @@ def update_task_status():
     for task in overdued_tasks:
         task.status = 'OVERDUE'
         task_overdue = client.records.update(task)
+
+
+@app.task
+def deadline_notification():
+    #
+    #          3h15m    3h       2h45m           0h
+    #  time____'________L________'_______________| <- DEADLINE
+    #          {  gap to notify  }
+    #
+    gap = datetime.now() + timedelta(hours=3)
+    threshold = timedelta(minutes=15, seconds=1)
+    start = '{0:%Y%m%dT%H%M%S}'.format(gap - threshold)
+    end = '{0:%Y%m%dT%H%M%S}'.format(gap + threshold)
+    task_obj = client.objects.get('task')
+    notify_tasks = client.records.query(task_obj, depth=2).filter(
+        status__eq='ACTIVE', deadline__gt=start, deadline__lt=end
+    )
+
+    for task in notify_tasks:
+        requests.post(
+            "{}/api/v1.0/mails/from_template/".format(settings.MAIL_SERVICE_URL),
+            json={
+                "mailbox": settings.SYSTEM_MAIL_ID,
+                "to": [task.executor['email']],
+                "template": "TASK_EXPIRING",
+                "data": {
+                    "task_id": task.id,
+                    "task_name": task.name
+                }
+            },
+            headers={'Authorization': get_service_token()}
+        )
+
+    return list(notify_tasks)
